d62f8644bf2fe9413c95b492cb2eaf4cf8f0917e
T Tyson <ttyson@blackstudentfund.org>
2020-12-27 14:11:09 -0500

    I want to test simplicity of using Pycharm to commit changes to Github

--- basic_code.py
+++ basic_code.py
@@ -1,6 +1,7 @@
 import pygame as pg
 #from sprite import Player
 import sys
+
 class Player:
     def __init__(self):
         #pg.sprite.Sprite.__init__(self)
@@ -8,23 +9,20 @@ class Player:
         self.image=pg.Surface((self.image_width, self.image_height))
         self.image.fill((200,0,0))
         self.image_rect=self.image.get_rect()#will appear in upper left of screen
-        self.image_rect.x=100; self.image_rect.y=100
+        self.image_rect.x=150; self.image_rect.y=100
         #self.speedx=0; self.speedy=0
         
         
-     def move(self): None
+    def move(self): ...
         
-     def update(self): ...
-
+    def update(self): ...
 
 
 class Game:
     def __init__(self):
         pg.init()
         self.screen_width=500; self.screen_height=500
-
         self.colors={"color1": (100,100,100), "color2": (150, 0,0)}
-
         self.screen=pg.display.set_mode((self.screen_width, self.screen_height))
         self.clock=pg.time.Clock()
         #self.player=Player()#Game is a composite
@@ -35,8 +33,6 @@ class Game:
             for event in pg.event.get():
                 if event.type==pg.QUIT:
                     sys.exit()
-
-
             self.screen.fill(self.colors["color1"])#put in a color
             #self.screen.blit(self.player.image, (100, 100))
             pg.display.flip()
