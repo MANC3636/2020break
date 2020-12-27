4366d0bce0cfa6745c8fa4efb0ba035051bdfdf1
T Tyson <ttyson@blackstudentfund.org>
2020-12-27 14:35:42 -0500

    I want to test simplicity of using Pycharm to commit changes to Github

---
+++ basic_functs.py
@@ -0,0 +1,34 @@
+import turtle as t
+t1=t.Pen()
+
+#functions for code reuse
+
+length=100#global scope
+length=45
+
+def square(color, length):#header
+    for x in "loop":
+        t1.color(color)
+        t1.fd(length)
+        t1.left(90)
+
+def move(length):
+    t1.up()
+    t1.fd(2*length)
+    t1.left(75)
+    t1.down()
+
+#call the function
+square("blue", 180)
+move(30)
+square("red", 50)
+square("purple", 5)
+
+
+
+
+
+
+
+
+t.mainloop()
