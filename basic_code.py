20b79a32bd08d6cd707f1ad93f187288f287d0b5
MANC3636 <73012287+MANC3636@users.noreply.github.com>
2020-12-27 14:08:09 -0500

    Update basic_code.py

--- basic_code.py
+++ basic_code.py
@@ -10,7 +10,11 @@ class Player:
         self.image_rect=self.image.get_rect()#will appear in upper left of screen
         self.image_rect.x=100; self.image_rect.y=100
         #self.speedx=0; self.speedy=0
-        #changed this version
+        
+        
+     def move(self): None
+        
+     def update(self): ...
 
 
 
